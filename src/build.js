97c30d5255dae2afb76d2cddf0c7eeb7b4baa72c
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2024-02-12 20:15:36 +0000

    debugging feed error

--- src/build.js
+++ src/build.js
@@ -88,6 +88,8 @@ async function build({ config, feeds, cache, writeCache = false }) {
                 const contentType = response.headers.get("content-type").split(";")[0];
 
                 if (!CONTENT_TYPES.includes(contentType)) {
+                    console.log(url);
+                    console.log(response);
                     console.log(contentType);
                     throw Error(`Feed at ${url} has invalid content-type.`);
                 }
